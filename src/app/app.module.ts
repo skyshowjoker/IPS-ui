6a519e462cdb3435733b73f418281fd0896c1f4a
perlicue <993768942@qq.com>
2022-06-30 11:39:25 +0800

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SettingComponent } from './setting/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
